c57256ab31a5f2e58ad96b551ad539c1c4216cca
Sam Julien <Sam.Julien@genesis-fs.com>
2015-04-08 11:12:09 -0700

    Add books listing.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150408180552) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
