b554727a26ba8445c5c9ba23c8062ce9fd8989a5
Sam Julien <Sam.Julien@genesis-fs.com>
2015-04-08 12:53:57 -0700

    added filter by rating

--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 class ListingBooksTest < ActionDispatch::IntegrationTest
   setup do
 	  Book.create!(title: 'Pragmatic Programmer', rating: 5)
-	  Book.create!(title: "Ender's Game", rating: 5)
+	  Book.create!(title: "Ender's Game", rating: 4)
   end
-  
+
   test 'listing books' do
   	get '/books'
 
@@ -13,5 +13,13 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
   	assert_equal Mime::JSON, response.content_type
 
   	assert_equal Book.count, JSON.parse(response.body).size
+  end
+
+  test 'lists top rated books' do
+  	get '/books?rating=5'
+
+  	assert_equal 200, response.status
+  	assert_equal Mime::JSON, response.content_type
+  	assert_equal 1, JSON.parse(response.body).size
   end
 end
